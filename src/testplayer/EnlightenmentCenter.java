7cc57e947964581b805636cbe85e840b749da5d7
Andrew Li <achyt28@gmail.com>
2021-01-12 16:25:34 -0500

    v3.1 of bidVote() only stop bidding when friendlyVotes>1500 logs have been commented out

--- src/testplayer/EnlightenmentCenter.java
+++ src/testplayer/EnlightenmentCenter.java
@@ -6,10 +6,8 @@ public class EnlightenmentCenter extends RobotPlayer{
 
 
     static int numberofunits = rc.getRobotCount();
-    static int numberofunitsproduced = 0;
-    static int numberofmuckrakersproduced = 0;
-    static int numberofslanderersproduced = 0;
-    static int numberofpoliticiansproduced = 0;
+    static int numberofunitsproduced, numberofmuckrakersproduced,
+    numberofslanderersproduced, numberofpoliticiansproduced = 0;
 
     static int ecIDTag = rc.getID() % 128;
     static void run() throws GameActionException {
@@ -122,6 +120,7 @@ public class EnlightenmentCenter extends RobotPlayer{
             numberofmuckrakersproduced++;
         }
     }
+
     static Direction getOptimalSpawn() throws GameActionException {
         Direction optimalDir = Direction.SOUTH;
         double optimalCost = Double.MIN_VALUE;
@@ -167,70 +166,68 @@ public class EnlightenmentCenter extends RobotPlayer{
             wonLastVote = true;
             if(winStreak<25) winStreak++;
             loseStreak = 0;
-            System.out.println("I won the last vote! Winstreak: " + winStreak);
+            //System.out.println("I won the last vote! Winstreak: " + winStreak);
         }
         else{
             wonLastVote = false;
             winStreak = 0;
             if(loseStreak<25) loseStreak++;
-            System.out.println("I lost the last vote :((. Losestreak: " + loseStreak);
+            //System.out.println("I lost the last vote :((. Losestreak: " + loseStreak);
         }
         friendlyVotes = rc.getTeamVotes();
 
 
-        if(friendlyVotes > 1500 || round-friendlyVotes > 1500)//if either we or enemy have already won the bidding game? unless ties...
+        if(friendlyVotes > 1500)// || round-friendlyVotes > 1500)//if either we or enemy have already won the bidding game? unless ties...
         {
-            //rc.bid(0);
-            System.out.println("The election has already been decided.");
+            //System.out.println("The election has already been decided.");
         }
         else if(!wonLastVote){ // we lost the last vote...
             double iCoef = 2;
 
             newBid = prevBid + (int) Math.ceil(Math.pow( (1/iCoef),(-loseStreak+1) ));
-            System.out.println("loseStreak: " + loseStreak + " prevBid:  " + prevBid + " newBid: " + newBid);
+            //System.out.println("loseStreak: " + loseStreak + " prevBid:  " + prevBid + " newBid: " + newBid);
             //increasing doubley (loseStreak is increasing while prevBid is increasing)
 
             int threshold = rc.getInfluence()/5; //our maximum we are willing to bid
             if(newBid < 1 && rc.canBid(1)){
                 rc.bid(1);
                 prevBid = 1;
-                System.out.println("Lost last vote, newBid<1 so lets bid the minimum, 1");
+                //System.out.println("Lost last vote, newBid<1 so lets bid the minimum, 1");
             }
             else if(newBid < threshold && rc.canBid(newBid)){
                 //dont want to be bankrupting ourselves so we have a threshold (max value we are willing to bid)
 
                 rc.bid(newBid);
                 prevBid = newBid;
-                System.out.println("Last vote lost, and we are less than the threshold, bid: " + newBid);
+                //System.out.println("Last vote lost, and we are less than the threshold, bid: " + newBid);
             }
             else if(newBid >= threshold && threshold>prevBid && rc.canBid(threshold)){
                 //bid the max we are willing to, also if its greater than our last bid
                 newBid = threshold;
                 rc.bid(newBid);
                 prevBid = newBid;
-                System.out.println("Last vote lost, and we are greater than the threshold, bid: " + newBid);
+                //System.out.println("Last vote lost, and we are greater than the threshold, bid: " + newBid);
             }
             else{
-                //rc.bid(0);
-                System.out.println("We lost the last vote, but we arent willing to bid more than last time so we bid 0");
+                //System.out.println("We lost the last vote, but we arent willing to bid more than last time so we bid 0");
             }
         }
         else{// we won the last vote!
             double dCoef = 1.7; //changeable
 
             newBid = (int) Math.ceil(-1*Math.pow( (1/dCoef),(-winStreak+2) ) + prevBid);
-            System.out.println("winStreak: " + winStreak + " prevBid:  " + prevBid + " newBid: " + newBid);
+            //System.out.println("winStreak: " + winStreak + " prevBid:  " + prevBid + " newBid: " + newBid);
             //decreasing doubley (winStreak is increasing while prevBid is decreasing)
 
             if(newBid < 1 && rc.canBid(1)){
                 rc.bid(1);
                 prevBid = 1;
-                System.out.println("Won last vote, newBid<1 so lets bid the minimum, 1");
+                //System.out.println("Won last vote, newBid<1 so lets bid the minimum, 1");
             }
             else if(rc.canBid(newBid)){
                 rc.bid(newBid);
                 prevBid = newBid;
-                System.out.println("Won last vote, lets bid " + newBid);
+                //System.out.println("Won last vote, lets bid " + newBid);
             }
 
         }
